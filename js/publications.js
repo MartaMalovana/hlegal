dfbe228d6144b9dbf1865dc955b6d09a00b93288
MartaMalovana <marta.malovana@gmail.com>
2022-11-18 23:24:02 +0200

    change pathes

--- js/publications.js
+++
@@ -1,8 +0,0 @@
-import publications from '../publications.json';
-
-function getPublications (arr) {
-    const reversedArr = [...arr].reverse();
-    console.log(reversedArr);
-};
-
-getPublications(JSON.parse(publications));
